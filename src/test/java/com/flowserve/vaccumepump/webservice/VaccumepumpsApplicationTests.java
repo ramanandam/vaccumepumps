c4ef5e77cf30a504721305c7e2d2b4eb920c2d8b
prasad nandam <prasad.nandam@gmail.com>
2024-11-26 21:37:59 +0530

    cleaned code

--- src/test/java/com/flowserve/vaccumepump/webservice/VaccumepumpsApplicationTests.java
+++ src/test/java/com/flowserve/vaccumepump/webservice/VaccumepumpsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class VaccumepumpsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	/*
+	 * @Test void contextLoads() { }
+	 */
 
 }
